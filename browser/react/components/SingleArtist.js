06e6a9d949c0ca4a1f4627102dd5172969303b89
Monica Choe <monicchoe@gmail.com>
2017-07-05 18:04:35 -0400

    finished writing sub-routes

--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Route} from 'react-router-dom';
 import axios from 'axios';
 import Songs from './Songs';
 import AllAlbums from './AllAlbums';
@@ -35,15 +36,39 @@ export default class SingleArtist extends Component {
     });
   }
 
+  // render () {
+  //   console.log('hi', this.state);
+  //   var information = this.state;
+  //   return (
+  //     <div>
+  //       <h3>{information.artist.name}</h3>
+  //       <AllAlbums albums={information.albums}/>
+  //       <Songs songs={information.songs}/>
+  //     </div>
+  //     );
+  // }
+
+
   render () {
-    console.log('hi', this.state);
-    var information = this.state;
+
+    const artist = this.state.artist; // or however you've named it
+    const artistAlbums = this.state.albums;
+    const artistSongs = this.state.songs;
     return (
       <div>
-        <h3>{information.artist.name}</h3>
-        <AllAlbums albums={information.albums}/>
-        <Songs songs={information.songs}/>
+        <h3>{ artist.name }</h3>
+        <ul className="nav nav-tabs">
+          <li><Link to={`/artists/${artist.id}/albums`}>ALBUMS</Link></li>
+          <li><Link to={`/artists/${artist.id}/songs`}>SONGS</Link></li>
+        </ul>
+
+          <Route path="/artists/:artistId/albums" render={
+  (routeProps) => <AllAlbums albums={artistAlbums} />
+} />
+          <Route path="/artists/:artistId/songs" render={
+  (routeProps) => <Songs songs={artistSongs} />
+} />
       </div>
-      );
+    );
   }
 }
