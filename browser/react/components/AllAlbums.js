af70854fbb972ca4f258799c140222adf735bd1b
vinayarv <vinayarv@users.noreply.github.com>
2017-07-05 16:01:43 -0400

    moving state down

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const fakeAlbums = [
+  {
+    name: 'Abbey Road',
+    id: 1,
+    imageUrl: 'http://fillmurray.com/300/300',
+    songs: [
+      {
+        id: 1,
+        name: 'Romeo & Juliette',
+        artists: [
+          { name: 'Bill' }
+        ],
+        genre: 'Funk',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      },
+      {
+        id: 2,
+        name: 'White Rabbit',
+        artists: [
+          { name: 'Bill' },
+          { name: 'Alice' }
+        ],
+        genre: 'Fantasy',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      },
+      {
+        id: 3,
+        name: 'Lucy in the Sky with Diamonds',
+        artists: [
+          { name: 'Bob' }
+        ],
+        genre: 'Space',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      }
+    ]
+  },
+  {
+    name: 'Yellow Submarine',
+    id: 2,
+    imageUrl: 'http://fillmurray.com/300/300',
+    songs: [
+      {
+        id: 4,
+        name: 'London Calling',
+        artists: [
+          { name: 'Bill' }
+        ],
+        genre: 'Punk',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      }
+    ]
+  }
+];
 
 export default class AllAlbums extends Component {
 
-  render () {
-    const albums = this.props.albums;
-    const selectAlbum = this.props.selectAlbum;
+  constructor(){
+    super();
+    this.state = { albums: []};
+  }
 
+  componentDidMount () {
+    axios.get('/api/albums/')
+      .then(res => res.data)
+      .then(albums => {
+        this.setState({ albums })
+      });
+  }
+
+  render () {
+    const albums = this.state.albums;
+    const selectAlbum = this.props.selectAlbum;
+    console.log(this.props);
     return (
       <div>
         <h3>Albums</h3>
--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -4,25 +4,17 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import { HashRouter, Route} from 'react-router-dom';
 
 export default class Main extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      albums: [],
       selectedAlbum: {}
     };
     this.selectAlbum = this.selectAlbum.bind(this);
     this.deselectAlbum = this.deselectAlbum.bind(this);
-  }
-
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
   }
 
   selectAlbum (albumId) {
@@ -43,13 +35,12 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
+        <HashRouter>
         <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
+          <Route path= '/' component={AllAlbums} exact />
+          <Route path= '/albums' component={AllAlbums} />
         </div>
+        </HashRouter>
         <Player />
       </div>
     );
